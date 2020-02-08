3e98c4fa0635641d124f36c42f4b3ef6a26ab4d6
Alexey Krasikov <krasikov11@gmail.com>
2020-02-08 09:27:39 +0300

    Added 3 levels, menu, credits

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour
+{
+    private bool gameHasEnded = false;
+    public float restartDelay = 1f;
+    public GameObject completeLevelUi;
+
+    public void CompleteLevel()
+    {
+        completeLevelUi.SetActive(true);
+    }
+    public void EndGame()
+    {
+        if (gameHasEnded == false)
+        {
+            gameHasEnded = true;
+            Invoke("Restart", restartDelay);
+        }
+    }
+
+    void Restart()
+    {
+        SceneManager.LoadScene("Level01");
+    }
+}
